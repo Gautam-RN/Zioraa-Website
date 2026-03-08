1524f76a8303c143342c0ebb228404cc8928ded3
Gautam-RN <gautamrnair01@gmail.com>
2026-03-08 16:12:23 +0530

    Refactor get_db function to use DATABASE_URL

--- db.py
+++ db.py
@@ -1,19 +1,13 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-
 def get_db():
     try:
-        scrt_db = os.getenv("DATABASE_URL")
-
-        conn = psycopg2.connect()
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
         print("DB OK!!")
         return conn, conn.cursor()
-
     except Exception as e:
         print("DB not connected!!")
         print("Error:", e)
         return None, None
-
