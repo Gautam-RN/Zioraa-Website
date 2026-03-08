db0fd75f4e458216d03ac8fe8d6117730f57edfc
Gautam-RN <gautamrnair01@gmail.com>
2026-03-08 16:14:52 +0530

    Modify get_db to use specific database credentials
    Updated database connection parameters in get_db function.

--- db.py
+++ db.py
@@ -1,12 +1,22 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 def get_db():
     try:
-        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+
+        conn = psycopg2.connect(
+    user="postgres.sbykicbslggbbmvwtmhl",
+    password="Z4zioraa_db",
+    host="aws-1-ap-northeast-1.pooler.supabase.com",
+    port=5432,
+    dbname="postgres"
+)
         print("DB OK!!")
         return conn, conn.cursor()
+
     except Exception as e:
         print("DB not connected!!")
         print("Error:", e)
